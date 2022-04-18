570a9b546676a34e3ba34046813b0f8cccef0db4
enrique.torralbo <enrique.torralbo@ct.uneatlantico.es>
2022-04-18 12:57:39 +0200

    minuto 14:41

--- start.js
+++ start.js
@@ -1,16 +1,49 @@
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  /* axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    params: {
+      _limit:5 
+    }
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err)); */
+
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit: 5')
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  /* axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'New todo',
+      completed: false
+    }
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err)); */
+  
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title: 'New todo',
+    completed: false
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios.put('https://jsonplaceholder.typicode.com/todos/1', {
+    title: 'Updated Todo',
+    completed: true
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
 }
 
 // DELETE REQUEST
